990b8d95f9fa29c34ff4ae4582c2e85457d59d9b
TheDIYGuy999 <msommer74@gmail.com>
2016-08-06 15:59:43 +0200

    Backchannel added, support for OLED display

--- RC_Transmitter.ino
+++ RC_Transmitter.ino
@@ -1,5 +1,8 @@
 // Micro RC Project. a tiny little 2.4GHz RC unit!
-// 3.3V, 8MHz Pro Mini or Pro Micro, 2.4GHz NRF24L01 radio module
+// 3.3V, 8MHz Pro Mini, 2.4GHz NRF24L01 radio module
+// SSD 1306 128 x 63 0.96" OLED
+
+float codeVersion = 0.9;
 
 //
 // =======================================================================================================
@@ -22,6 +25,8 @@
 #include <SimpleTimer.h> // https://github.com/jfturcot/SimpleTimer
 #include <LegoIr.h> // https://github.com/TheDIYGuy999/LegoIr
 #include <statusLED.h> // https://github.com/TheDIYGuy999/statusLED
+#include "U8glib.h"
+
 
 // Tabs
 #include "readVCC.h"
@@ -42,6 +47,9 @@ const int maxVehicleNumber = 5;
 // the ID number of the used "radio pipe" must match with the programmed ID on the vehicle receiver!
 const uint64_t pipeOut[maxVehicleNumber] = { 0xE9E8F0F0B1LL, 0xE9E8F0F0B2LL, 0xE9E8F0F0B3LL, 0xE9E8F0F0B4LL, 0xE9E8F0F0B5LL };
 
+// Hardware configuration: Set up nRF24L01 radio on hardware SPI bus & pins 7 (CE) & 8 (CSN)
+RF24 radio(7, 8);
+
 // The size of this struct should not exceed 32 bytes
 struct RcData {
   byte axis1; // Aileron (Steering for car)
@@ -49,20 +57,30 @@ struct RcData {
   byte axis3; // Throttle
   byte axis4; // Rudder
   boolean mode1 = false; // Speed limitation (left Joystick button)
-  boolean mode2 = false; // Nothing (right joystick button)
+  boolean mode2 = false; // Acceleration limitation (right joystick button)
 };
-
 RcData data;
 
-// Hardware configuration: Set up nRF24L01 radio on hardware SPI bus & pins 7 (CE) & 8 (CSN)
-RF24 radio(7, 8);
+// This struct defines data, which are embedded inside the ACK payload
+struct ackPayload {
+  float vcc; // vehicle vcc voltage
+  float batteryVoltage; // vehicle battery voltage
+  boolean batteryOk; // the vehicle battery voltage is OK!
+};
+ackPayload payload;
+
+// Did the receiver acknowledge the sent data?
+boolean transmissionState;
 
 // LEGO powerfunctions IR
 LegoIr pf;
 const int maxAddressPF = 4;
 
-// Battery
-boolean batteryOK = false;
+// TX voltages
+boolean batteryOkTx = false;
+#define BATTERY_DETECT_PIN A7 // The 20k & 10k battery detection voltage divider is connected to pin A3
+float txVcc;
+float txBatt;
 
 // Timer
 SimpleTimer timer;
@@ -92,6 +110,10 @@ statusLED redLED(false); // red: ON = battery empty
   That would qualify as a debounced raising edge*/
 #define DRE(signal, state) (state=(state<<1)|(signal&1)&15)==7
 
+// OLED display
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_FAST);  // I2C / TWI  FAST instead of NONE = 400kHz I2C!
+int activeScreen = 0; // the currently displayed screen number (0 = splash screen)
+
 //
 // =======================================================================================================
 // RADIO SETUP
@@ -104,15 +126,18 @@ void setupRadio() {
   radio.setPALevel(RF24_PA_HIGH);
   radio.setDataRate(RF24_250KBPS);
   radio.setAutoAck(true);                  // Ensure autoACK is enabled
-  radio.setRetries(1, 5);                  // 1x250us delay (blocking!!), max. 5 retries
-  radio.setCRCLength(RF24_CRC_8);          // Use 8-bit CRC for performance*/
+  radio.enableAckPayload();
+  radio.enableDynamicPayloads();
+  radio.setRetries(5, 5);                  // 5x250us delay (blocking!!), max. 5 retries
+  //radio.setCRCLength(RF24_CRC_8);          // Use 8-bit CRC for performance*/
+
 #ifdef DEBUG
   radio.printDetails();
   delay(3000);
 #endif
   radio.openWritingPipe(pipeOut[vehicleNumber - 1]); // Vehicle Number 1 = Array number 0, so -1!
 
-  // All axis to neutral position
+  // All axes to neutral position
   data.axis1 = 50;
   data.axis2 = 50;
   data.axis3 = 50;
@@ -128,7 +153,7 @@ void setupRadio() {
 //
 
 void setupPowerfunctions() {
-  int pfChannel = vehicleNumber - 1;  // channel 0 - 3 is labbelled as 1 - 4 on the LEGO devices!
+  int pfChannel = vehicleNumber - 1;  // channel 0 - 3 is labelled as 1 - 4 on the LEGO devices!
 
   if (pfChannel > 3) pfChannel = 3;
 
@@ -165,6 +190,21 @@ void setup() {
   // LEGO Powerfunctions setup
   setupPowerfunctions();
 
+  // Display setup
+  u8g.setRot180(); // flip screen, if required
+  u8g.setFontRefHeightExtendedText();
+  u8g.setDefaultForegroundColor();
+  u8g.setFontPosTop();
+  u8g.setFont(u8g_font_6x10);
+
+  // Splash screen
+  checkBattery();
+  activeScreen = 0; // 0 = splash screen active
+  drawDisplay();
+  activeScreen = 1; // switch to the main screen
+  delay(2500);
+  drawDisplay();
+
   // Periodically called functions
   timer.setInterval(20, readButtons); // Read buttons every 20ms
 
@@ -181,11 +221,13 @@ void readButtons() {
   // Left button (integrated in Joystick)
   if (DRE(digitalRead(JOYSTICK_BUTTON_LEFT), leftJoystickButtonState)) {
     data.mode1 = !data.mode1;
+    drawDisplay();
   }
 
   // right button (integrated in Joystick)
   if (DRE(digitalRead(JOYSTICK_BUTTON_RIGHT), rightJoystickButtonState)) {
     data.mode2 = !data.mode2;
+    drawDisplay();
   }
 
   // left button S2: Channel selection
@@ -194,11 +236,13 @@ void readButtons() {
     if (vehicleNumber > maxVehicleNumber) vehicleNumber = 1;
     setupRadio(); // Re-initialize the radio with the new pipe address
     setupPowerfunctions(); // Re-initialize the IR transmitter with the new channel address
+    drawDisplay();
   }
 
   // right button S3: Change transmission mode. Radio <> IR
   if (DRE(digitalRead(BUTTON_RIGHT), rightButtonState)) {
     IrMode = !IrMode;
+    drawDisplay();
   }
 }
 
@@ -212,22 +256,6 @@ void readJoysticks() {
   data.axis2 = map(analogRead(A1), 0, 1023, 0, 100); // Elevator
   data.axis3 = map(analogRead(A2), 0, 1023, 0, 100); // Throttle
   data.axis4 = map(analogRead(A3), 0, 1023, 0, 100); // Rudder
-}
-
-//
-// =======================================================================================================
-// LED
-// =======================================================================================================
-//
-
-void led() {
-
-  // Red LED (ON = battery empty, number of pulses are indicating the vehicle number)
-  if (batteryOK) {
-    redLED.flash(140, 150, 500, vehicleNumber); // ON, OFF, PAUSE, PULSES
-  } else {
-    redLED.on(); // Always ON = battery low voltage
-  }
 }
 
 //
@@ -276,8 +304,6 @@ void transmitLegoIr() {
 
       // then transmit IR data
       pf.combo_pwm(pwm[1], pwm[0]); // red and blue in one IR package
-      //pf.single_pwm(RED, pwm[0]); // or each color separately
-      //pf.single_pwm(BLUE, pwm[1]);
     }
   }
 }
@@ -290,21 +316,54 @@ void transmitLegoIr() {
 
 void transmitRadio() {
 
-  // Send radio data
-  radio.write(&data, sizeof(data));
+  static boolean previousTransmissionState;
+  static float previousRxVcc;
+  static float previousRxVbatt;
+  static unsigned long previousSuccessfulTransmission;
 
-  // Data successfully transmitted?
-  if (radio.write(&data, sizeof(data)) ) {
-    greenLED.flash(30, 100, 0, 0); //30, 100
-#ifdef DEBUG
-    Serial.println("Data successfully transmitted");
-#endif
-  } else {
+  // Send radio data and check if transmission was successful
+  if (radio.write(&data, sizeof(struct RcData)) ) {
+    if (radio.isAckPayloadAvailable()) {
+      radio.read(&payload, sizeof(struct ackPayload)); // read the payload, if available
+      previousSuccessfulTransmission = millis();
+    }
+  }
+
+  // if the transmission was not confirmed after > 150ms...
+  if (millis() - previousSuccessfulTransmission > 150) {
     greenLED.on();
+    transmissionState = false;
+    memset(&payload, 0, sizeof(payload)); // clear the payload array, if transmission error
 #ifdef DEBUG
     Serial.println("Data transmission error, check receiver!");
 #endif
   }
+  else {
+    greenLED.flash(30, 100, 0, 0); //30, 100
+    transmissionState = true;
+#ifdef DEBUG
+    Serial.println("Data successfully transmitted");
+#endif
+  }
+
+  // refresh transmission state on the display, if changed
+  if (transmissionState != previousTransmissionState) {
+    previousTransmissionState = transmissionState;
+    drawDisplay();
+  }
+
+  // refresh Rx Vcc on the display, if changed more than +/- 0.05V
+  if (payload.vcc - 0.05 >= previousRxVcc || payload.vcc + 0.05 <= previousRxVcc) {
+    previousRxVcc = payload.vcc;
+    drawDisplay();
+  }
+
+  // refresh Rx V Batt on the display, if changed more than +/- 0.2V
+  if (payload.batteryVoltage - 0.5 >= previousRxVbatt || payload.batteryVoltage + 0.5 <= previousRxVbatt) {
+    previousRxVbatt = payload.batteryVoltage;
+    drawDisplay();
+  }
+
 
 #ifdef DEBUG
   Serial.print(data.axis1);
@@ -321,28 +380,137 @@ void transmitRadio() {
 
 //
 // =======================================================================================================
-// CHECK BATTERY VOLTAGE
+// LED
+// =======================================================================================================
+//
+
+void led() {
+
+  // Red LED (ON = battery empty, number of pulses are indicating the vehicle number)
+  if (batteryOkTx && (payload.batteryOk || IrMode || !transmissionState) ) {
+    redLED.flash(140, 150, 500, vehicleNumber); // ON, OFF, PAUSE, PULSES
+  } else {
+    redLED.on(); // Always ON = battery low voltage (Rx or Tx)
+  }
+}
+
+//
+// =======================================================================================================
+// CHECK TX BATTERY VOLTAGE
 // =======================================================================================================
 //
 
 void checkBattery() {
-  float batteryVolt = readVcc() / 1000.0 ;
 
-  if (batteryVolt >= 3.0) {
-    batteryOK = true;
+  txBatt = analogRead(BATTERY_DETECT_PIN) / 103.33; // 1023steps / 9.9V = 103.33
+  txVcc = readVcc() / 1000.0 ;
+
+  if (txBatt >= 4.4) {
+    batteryOkTx = true;
 #ifdef DEBUG
     Serial.print(batteryVolt);
-    Serial.println(" V. Battery OK");
+    Serial.println(" Tx battery OK");
 #endif
   } else {
-    batteryOK = false;
+    batteryOkTx = false;
 #ifdef DEBUG
     Serial.print(batteryVolt);
-    Serial.println(" V. Battery empty!");
+    Serial.println(" Tx battery empty!");
 #endif
   }
 }
 
+//
+// =======================================================================================================
+// DRAW DISPLAY
+// =======================================================================================================
+//
+
+void drawDisplay() {
+
+  u8g.firstPage();  // clear screen
+  do {
+    switch (activeScreen) {
+      case 0: // Screen # 0 splash screen-----------------------------------
+
+        u8g.drawStr(3, 10, "Micro RC Controller");
+
+        // Dividing Line
+        u8g.drawLine(0, 13, 128, 13);
+
+        // Software version
+        u8g.setPrintPos(3, 30);
+        u8g.print("SW Version: ");
+        u8g.print(codeVersion);
+
+        u8g.setPrintPos(3, 45);
+        u8g.print("created by:");
+        u8g.setPrintPos(3, 55);
+        u8g.print("TheDIYGuy999");
+
+        break;
+
+      case 1: // Screen # 1 main screen-------------------------------------
+
+        // screen dividing lines ----
+        u8g.drawLine(0, 13, 128, 13);
+        u8g.drawLine(64, 0, 64, 64);
+
+        // Tx: data ----
+        u8g.setPrintPos(3, 10);
+        if (IrMode) u8g.print("Tx: IR   ");
+        else u8g.print("Tx: 2.4G ");
+        u8g.print(vehicleNumber);
+
+        u8g.setPrintPos(3, 25);
+        u8g.print("Vcc: ");
+        u8g.print(txVcc);
+
+        u8g.setPrintPos(3, 35);
+        u8g.print("Bat: ");
+        u8g.print(txBatt);
+
+        // Rx: data. Only display the following content, if in radio mode ----
+        if (!IrMode) {
+          u8g.setPrintPos(68, 10);
+          if (transmissionState) {
+            u8g.print("Rx: OK");
+          }
+          else {
+            u8g.print("Rx: ??");
+          }
+
+          u8g.setPrintPos(3, 45);
+          u8g.print("Mode 1: ");
+          u8g.print(data.mode1);
+
+          u8g.setPrintPos(3, 55);
+          u8g.print("Mode 2: ");
+          u8g.print(data.mode2);
+
+          if (transmissionState) {
+            u8g.setPrintPos(68, 25);
+            u8g.print("Vcc: ");
+            u8g.print(payload.vcc);
+
+            u8g.setPrintPos(68, 35);
+            u8g.print("Bat: ");
+            u8g.print(payload.batteryVoltage);
+
+            u8g.setPrintPos(68, 55);
+            if (payload.batteryOk) {
+              u8g.print("Bat. OK ");
+            }
+            else {
+              u8g.print("Low Bat. ");
+            }
+          }
+        }
+
+        break;
+    }
+  } while ( u8g.nextPage() ); // show display queue
+}
 
 //
 // =======================================================================================================
@@ -358,7 +526,7 @@ void loop() {
   // Read joysticks
   readJoysticks();
 
-  // Send data via IR or 2.4GHz radio
+  // Transmit data via infrared or 2.4GHz radio
   if (IrMode) {
     transmitLegoIr();
   } else {
--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 - 2.4GHz radio for RC cars, helicopters etc.
 - 4 analog axis, 2 mode switches (joystick buttons)
 - 5 seletcable vehicle addresses, so you can use it for more than one vehicle
-- 3.3V, 8MHz Pro Mini or Pro Micro, 2.4GHz NRF24L01 radio module
+- 3.3V, 8MHz Pro Mini, 2.4GHz NRF24L01 radio module
+- Support for 0.96" I2C OLED
 - very compact
 
 ## Usage
